a5ef40917997b4e8a7913f3e738f7b8469ef4576
Ryan Barry <rbarry@redhat.com>
2015-02-26 21:05:08 -0700

    Add a working service factory.
    Obviously needs to be more generic later, and learn how to
    introspect arguments, but we can export generic function names
    on dbus through closures, which are called dynamically, with
    arbitrary arguments.
    
    Signed-off-by: Ryan Barry <rbarry@redhat.com>

--- src/service.py
+++ src/service.py
@@ -5,17 +5,60 @@ import dbus
 import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
 import gobject
+import augeas
+from pprint import pprint
+
+BUS_NAME = "org.augeasproject.Augeas"
+BUS_PATH = "/org/augeasproject/Augeas"
 
 DBusGMainLoop(set_as_default=True)
 
-class DBusBackend(dbus.service.Object):
-    name = None
-    path = None
+class Test(object):
+    def get(self, path):
+        import augeas
+        print "Called"
+        print path
+        return augeas.augeas().get(path)
+
+class DBusFactory(object):
+    name = BUS_NAME
 
     def __init__(self, name, leaf="/Service"):
-        bus = dbus.SessionBus()
-        uid = dbus.service.BusName(name, bus=bus)
         path = "/" + name.replace(".", "/")
-        dbus.service.Object.__init__(self, uid, leaf)
         self.path = path
         self.name = name
+
+    def service_factory(self, method):
+        path = self.path
+        name = self.name
+        leaf = "/Service"
+
+        class Service(dbus.service.Object):
+            def __init__(self):
+                bus = dbus.service.BusName(name, bus=dbus.SessionBus())
+                path = "/" + name.replace(".", "/")
+                dbus.service.Object.__init__(self, bus, leaf)
+
+            def closure(self, path):
+                return method(path)
+
+            closure.__name__ = method.__name__
+            dbus.service.method(name)(closure)
+
+        return Service()
+
+if __name__ == "__main__":
+    print sys.argv
+    if "-d" in sys.argv:
+        DBusGMainLoop(set_as_default=True)
+        loop = gobject.MainLoop()
+        print "listening ..."
+        d = DBusFactory(BUS_NAME)
+        x = d.service_factory(Test().get)
+        loop.run()
+
+    elif "-c" in sys.argv:
+        bus = dbus.SessionBus()
+        helloservice = bus.get_object(BUS_NAME, BUS_PATH+"/Service")
+        print helloservice.get("/files/etc/hostname/hostname")
+
