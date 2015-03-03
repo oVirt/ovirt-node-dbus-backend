ec5f91b4553ba8c638b16cafc072667b5bc0f66d
Ryan Barry <rbarry@redhat.com>
2015-03-02 18:08:46 -0700

    Use the Dbus system bus instead.
    Export classes with class-based names, and iterate over
    them to populate the bus
    
    Signed-off-by: Ryan Barry <rbarry@redhat.com>

--- src/service.py
+++ src/service.py
@@ -8,8 +8,8 @@ import gobject
 import augeas
 from pprint import pprint
 
-BUS_NAME = "org.augeasproject.Augeas"
-BUS_PATH = "/org/augeasproject/Augeas"
+BUS_NAME = "org.ovirt.node"
+BUS_PATH = "/org.ovirt.node"
 
 DBusGMainLoop(set_as_default=True)
 
@@ -21,11 +21,17 @@ class Test(object):
         print path
         return augeas.augeas().get(path)
 
+    def configure(self, path):
+        return self.get(path)
+
+    def transaction(self):
+        print "Running transaction"
+
 
 class DBusFactory(object):
     name = BUS_NAME
 
-    def __init__(self, name, leaf="/Service"):
+    def __init__(self, name):
         path = "/" + name.replace(".", "/")
         self.path = path
         self.name = name
@@ -33,23 +39,36 @@ class DBusFactory(object):
     def service_factory(self, method):
         path = self.path
         name = self.name
-        leaf = "/Service"
+        leaf = "/org/ovirt/node/%s/%s" % (method.im_class.__name__,
+                                          method.__name__)
 
         class Service(dbus.service.Object):
             def __init__(self):
-                bus = dbus.service.BusName(name, bus=dbus.SessionBus())
+                bus = dbus.service.BusName(name, bus=dbus.SystemBus())
                 path = "/" + name.replace(".", "/")
                 dbus.service.Object.__init__(self, bus, leaf)
 
             def closure(self, path):
-                return method(path)
+                return method(method.im_class(), path)
 
             closure.__name__ = method.__name__
             locals()[method.__name__] = closure
+            print "Exporting %s" % method.__name__
             dbus.service.method(name, in_signature="s")(
                 locals()[method.__name__])
 
         return Service()
+
+class ClassParser(object):
+
+    def __init__(self, cls):
+        self._cls = cls
+
+    @property
+    def methods(self):
+       funcs = [getattr(self._cls, func) for func in dir(self._cls) if
+                func.startswith("configure")]
+       return funcs
 
 if __name__ == "__main__":
     print sys.argv
@@ -58,12 +77,13 @@ if __name__ == "__main__":
         loop = gobject.MainLoop()
         print "listening ..."
         d = DBusFactory(BUS_NAME)
-        x = d.service_factory(Test().get)
+        c = ClassParser(Test)
+        [d.service_factory(x) for x in c.methods]
         loop.run()
 
     elif "-c" in sys.argv:
-        bus = dbus.SessionBus()
-        obj = bus.get_object(BUS_NAME, "/Service")
-        helloservice = dbus.Interface(obj, "org.augeasproject.Augeas")
-        print helloservice.get("/files/etc/hostname/hostname")
-        print helloservice.get("/files/etc/resolv.conf/nameserver[1]")
+        bus = dbus.SystemBus()
+        obj = bus.get_object(BUS_NAME, "/org/ovirt/node/Test/configure")
+        helloservice = dbus.Interface(obj, "org.ovirt.node")
+        print helloservice.configure("/files/etc/hostname/hostname")
+        print helloservice.configure("/files/etc/resolv.conf/nameserver[1]")
