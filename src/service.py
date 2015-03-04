9bba328c7d05298a119632a6943640991b910207
Ryan Barry <rbarry@redhat.com>
2015-03-03 18:13:20 -0700

    Dynamic service creation from functions with arbitrary arguments.
    Needs a lot of metaprogramming and class instance substitution
    here and there to make it work properly.
    
    Signed-off-by: Ryan Barry <rbarry@redhat.com>

--- src/service.py
+++ src/service.py
@@ -5,6 +5,7 @@ import dbus
 import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
 import gobject
+from decorator import decorator
 
 BUS_NAME = "org.ovirt.node"
 BUS_PATH = "/org.ovirt.node"
@@ -19,14 +20,22 @@ class Test(object):
     can be easily tested
     """
 
+    output = "Not run yet"
+
     def get(self, path):
         import augeas
         print "Called"
         print path
         return augeas.augeas().get(path)
 
-    def configure(self, path):
+    def configure_one(self, path):
         self.output = self.get(path)
+
+    def configure_multi(self, a, b):
+        self.output = "%s, %s" % (a, b)
+
+    def configure_arr(self, x):
+        self.output = "%s" % x
 
     def transaction(self):
         print "Running transaction"
@@ -41,73 +50,117 @@ class DBusFactory(object):
     """
     name = BUS_NAME
 
-    def __init__(self, name):
-        path = "/" + name.replace(".", "/")
-        self.path = path
+    def __init__(self, name, cls):
+        self.cls = cls
         self.name = name
 
-    def service_factory(self, method):
+    def service_factory(self):
         """
-        Introspect a method passed in and wrap it. We assume that
+        Introspect a class passed in and wrap it. We assume that
         the method needs "self" as the first argument, since static or
         standalone methods would not need to be passed into a factory
         to easily get on dbus
         """
 
-        path = self.path
         name = self.name
-        leaf = "%s/%s/%s" % (path, method.im_class.__name__,
-                             method.__name__)
+        cls = self.cls
 
         class Service(dbus.service.Object):
             def __init__(self):
+                path = "/" + name.replace(".", "/")
+                leaf = "%s/%s" % (path, cls.__name__)
                 bus = dbus.service.BusName(name, bus=dbus.SystemBus())
                 dbus.service.Object.__init__(self, bus, leaf)
 
-            def closure(self, path):
-                return method(method.im_class(), path)
+            def methods(cls_iter=self):
+                """
+                Loop over whatever class is passed in and get a list of all
+                the methods which start with configur, then return them
+                """
+                funcs = [getattr(cls_iter, func) for func in dir(cls_iter) if
+                         func.startswith("configure")]
+                return funcs
 
-            # In order to get the right name on the bus on EL7 and later
-            # distros, re-set the name, and create a local variable with the
-            # same name, so dbus's decorator can infer it
-            closure.__name__ = method.__name__
-            locals()[method.__name__] = closure
-            print "Exporting %s" % method.__name__
-            dbus.service.method(name, in_signature="s")(
-                locals()[method.__name__])
+            # Because dbus-python doesn't like to export functions with varargs
+            # we need to create
+            def closure(method):
+                # Use the decorator here because it preserves the argspec,
+                # and dbus-python doesn't deal with varargs very well unless
+                # we do this
+                @decorator
+                def dec(func, *args, **kwargs):
+                    if not isinstance(args[0], method.im_class):
+                        args_list = list(args)
+                        args_list[0] = self.cls.instance
+                        args = args_list
+                    return func(*args)
+                return dec
+
+            for method in methods(self.cls):
+                # In order to get the right name on the bus on EL7 and later
+                # distros, re-set the name, and add it to the class, because
+                # decorator parsing order needs it to be realized at
+                # instantiation, and we can't dynamically add them afterwards
+                # and have them show up
+                closed = closure(method)(method)
+                closed.__name__ = method.__name__
+                setattr(self, method.__name__, closed)
+
+            for method in methods():
+                # Add to local variables so we can export it but still do
+                # some metaprogramming, like resetting the class name, which
+                # we can't do without referencing it directly
+                locals()[method.__name__] = method
+                locals()[method.__name__].im_class = cls.cls
+                print "Exporting %s on %s" % (method.__name__, name)
+                dbus.service.method(name)(locals()[method.__name__])
 
         return Service()
 
 
-class ConfigDefaultsParser(object):
+class ConfigDefaultsWrapper(object):
     """
     A class which steps through classes in the same format as
     config.defaults classes. Namely, we want to export methods which
     start with configure, and we want to make sure the transaction
     is run when they're triggered.
+
+    Run through the list and dynamically add them to this class
+    inside closures
     """
 
     def __init__(self, cls):
-        self._cls = cls
+        self.cls = cls
+        self.__name__ = cls.__name__
+        self.populate()
+        self.instance = cls()
 
-    @property
-    def methods(self):
-        funcs = [getattr(self._cls, func) for func in dir(self._cls) if
+    def populate(self):
+        funcs = [getattr(self.cls, func) for func in dir(self.cls) if
                  func.startswith("configure")]
 
-        wrapped_funcs = []
         for func in funcs:
             # Run in another closure so the transaction can be triggered
             # in one go from dbus
-            def wrapped(cls, x):
-                func(cls, x)
-                return cls.transaction()
+            def wrapper(cls):
+                @decorator
+                def dec(func, *args, **kwargs):
+                    # Sub out self for the actual instance so we can
+                    # run the transaction
+                    if isinstance(args[0], self.cls):
+                        args_list = list(args)
+                        args_list[0] = self.instance
+                        args = args_list
+                    func(*args)
+                    return self.instance.transaction()
+                return dec
 
             # Pass in the real attributes make the wrapper transparent
-            wrapped.im_class = func.im_class
+            wrapped = wrapper(self.cls)(func.im_func)
+            wrapped.im_class = self.cls
             wrapped.__name__ = func.__name__
-            wrapped_funcs.append(wrapped)
-        return wrapped_funcs
+            setattr(self, func.__name__, wrapped)
+
 
 if __name__ == "__main__":
     print sys.argv
@@ -115,14 +168,17 @@ if __name__ == "__main__":
         DBusGMainLoop(set_as_default=True)
         loop = gobject.MainLoop()
         print "listening ..."
-        d = DBusFactory(BUS_NAME)
-        c = ConfigDefaultsParser(Test)
-        [d.service_factory(x) for x in c.methods]
+        c = ConfigDefaultsWrapper(Test)
+        d = DBusFactory(BUS_NAME, c)
+        d.service_factory()
         loop.run()
 
     elif "-c" in sys.argv:
         bus = dbus.SystemBus()
-        obj = bus.get_object(BUS_NAME, "/org/ovirt/node/Test/configure")
+        obj = bus.get_object(BUS_NAME, "/org/ovirt/node/Test")
         helloservice = dbus.Interface(obj, "org.ovirt.node")
-        print helloservice.configure("/files/etc/hostname/hostname")
-        print helloservice.configure("/files/etc/resolv.conf/nameserver[1]")
+        print helloservice.configure_one("/files/etc/hostname/hostname")
+        print helloservice.configure_one("/files/etc/resolv.conf/nameserver[1]"
+                                         )
+        print helloservice.configure_multi(1, 2)
+        print helloservice.configure_arr([1, 2])
