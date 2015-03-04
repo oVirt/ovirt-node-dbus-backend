6515b4af7569bfac43d46b55c827394652642ab4
Ryan Barry <rbarry@redhat.com>
2015-03-04 14:39:30 -0700

    Add a transaction wrapper
    Signed-off-by: Ryan Barry <rbarry@redhat.com>

--- src/wrappers.py
+++ src/wrappers.py
@@ -18,8 +18,11 @@
 # MA  02110-1301, USA.  A copy of the GNU General Public License is
 # also available at http://www.gnu.org/copyleft/gpl.html.
 
+import dbus
 from decorator import decorator
-
+import logging
+from ovirt.node.utils.console import TransactionProgress
+from ovirt.node.utils import Transaction
 
 class ConfigDefaultsWrapper(object):
     """
@@ -48,11 +51,44 @@ class ConfigDefaultsWrapper(object):
             # we keep the original argspec and dbus-python is happy
             @decorator
             def dec(func, *args, **kwargs):
+                def dbus_unwrapper(arg):
+                    if type(arg) is dbus.String:
+                        return "%s" % arg
+                    else:
+                        return arg
+                wrapped_args = [dbus_unwrapper(arg) for arg in args]
+                args = wrapped_args
                 func(*args)
-                return self.instance.transaction()
+                return TransactionWrapper(self.instance.transaction(),
+                                          is_dry=False).run()
 
             # Pass in the real attributes make the wrapper transparent
             wrapped = dec(func.im_func)
             wrapped.im_class = self.cls
             wrapped.__name__ = func.__name__
             setattr(self, func.__name__, wrapped)
+
+
+class TransactionWrapper(TransactionProgress):
+    """
+    Run a transaction inside Dbus. Return the output as a string
+    """
+
+    def __init__(self, transaction, is_dry, initial_text=""):
+        print transaction
+        super(TransactionWrapper, self).__init__(transaction, is_dry,
+                                                 initial_text)
+    def _print_func(self, txt):
+        pass
+
+    def run(self):
+        if self.transaction:
+            self.logger.debug("Initiating transaction")
+            super(TransactionWrapper, self).run_transaction()
+            return self.texts
+        else:
+            self.update("There were no changes, nothing to do.")
+
+    def __print_title(self):
+        self.texts.extend([self.transaction.title,
+                           "-" * len(self.transaction.title)])
